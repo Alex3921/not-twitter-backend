2156b2f2e41ffab024640f3e9a83f0855fc42e52
alexandru <boncut.alex10@gmail.com>
2021-12-15 23:46:39 -0600

    Add User resource

--- db/migrate/20211208054510_create_users.rb
+++ db/migrate/20211208054510_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-
+      t.string :username
+      t.string :email
+      t.string :bio
+      t.date :birthdate
+      t.string :location
+      t.string :name
       t.timestamps
     end
   end
