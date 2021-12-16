0f866677ba52d830bd989c23b2dcce4bd329e934
alexandru <boncut.alex10@gmail.com>
2021-12-15 23:46:52 -0600

    Add Post resource

---
+++ db/migrate/20211216044302_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :content
+      t.references :user
+      t.integer :reply_count
+      t.integer :in_reply_to_post_id
+      t.integer :in_reply_to_author_id
+      t.string :in_reply_to_author_name
+
+      t.timestamps
+    end
+  end
+end
