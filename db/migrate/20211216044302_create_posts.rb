989e9a7eb48e0786ada6da4fbeda451e26baa860
alexandru <boncut.alex10@gmail.com>
2021-12-16 18:27:19 -0600

    Add default value to reply_count

--- db/migrate/20211216044302_create_posts.rb
+++ db/migrate/20211216044302_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :content
       t.references :user
-      t.integer :reply_count
+      t.integer :reply_count, default: 0
       t.integer :in_reply_to_post_id
       t.integer :in_reply_to_author_id
       t.string :in_reply_to_author_name
