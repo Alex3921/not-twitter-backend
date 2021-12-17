3b9ac33d70baab278c117fd913d416ad3efea6b5
alexandru <boncut.alex10@gmail.com>
2021-12-16 23:40:48 -0600

    Add Retweet resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_044302) do
+ActiveRecord::Schema.define(version: 2021_12_17_050734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2021_12_16_044302) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "retweets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_retweets_on_post_id"
+    t.index ["user_id"], name: "index_retweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -38,4 +47,6 @@ ActiveRecord::Schema.define(version: 2021_12_16_044302) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "retweets", "posts"
+  add_foreign_key "retweets", "users"
 end
