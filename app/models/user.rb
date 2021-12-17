1354731aa4137e708fd8d0e3fc097f6443a099b7
alexandru <boncut.alex10@gmail.com>
2021-12-16 23:43:27 -0600

    Add a replies method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :retweets
   has_many :reposts, through: :retweets, source: :post
 
+  def replies
+    self.posts.select{|post| post.in_reply_to_post_id != nil}
+  end
+
 end
