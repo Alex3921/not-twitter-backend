cc9d723352172ecea677177e5f5c0d0a12560123
alexandru <boncut.alex10@gmail.com>
2021-12-17 23:05:38 -0600

    Add relationship to "like" & "retweet"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :user_id
+
   has_many :replies, class_name: "Post", foreign_key: :in_reply_to_post_id
+  has_many :likes
+  has_many :retweets
 end
