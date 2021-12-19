ac69ff7534925f673fb75c0185b4c5ad6a2f5438
alexandru <boncut.alex10@gmail.com>
2021-12-18 23:54:53 -0600

    Add index and show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def index
+    user = User.all
+    render json: user
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    render json: user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :bio, :birthdate, :location, :name)
+  end
 end
