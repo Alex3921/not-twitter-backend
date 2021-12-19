e5e33b606312057cfbe2cbbf41460068af9c256e
alexandru <boncut.alex10@gmail.com>
2021-12-18 23:57:46 -0600

    User has_many: followers, has_many: followed_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :likes
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  
   has_many :liked_posts, through: :likes, source: :post
   has_many :retweets
   has_many :reposts, through: :retweets, source: :post
