a2068f5e24e7c65348331b2ae1df2567b3e86d3a
alexandru <boncut.alex10@gmail.com>
2021-12-18 23:56:25 -0600

    Add followers and followed users relationships

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :bio, :birthdate, :location, :name
 
-  has_many :posts
+  has_many :followers
+  has_many :followed_users
+  # has_many :posts
 end
+
+
+
+
+
+
+
+
