73081ae5d5e9ee649d0844a4948e9339bef59f85
alexandru <boncut.alex10@gmail.com>
2021-12-24 23:35:44 -0600

    Test followed_users_posts method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
 
   def index
-    posts = Post.all
-    render json: posts
+    user = User.find_by(id: params[:user_id])
+    followed_users_posts = user.followed_users_posts
+    render json: followed_users_posts
   end
+
 end
