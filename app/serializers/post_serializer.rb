505bad3674964fb1467e9440c5d8185c63774149
alexandru <boncut.alex10@gmail.com>
2021-12-24 23:36:25 -0600

    Add relationship: belongs_to :author

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :content, :in_reply_to_post_id, :in_reply_to_author_id, :reply_count, :in_reply_to_author_name
+
+  belongs_to :author
 end
