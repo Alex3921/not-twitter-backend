4039614418cc1cc936c0f4d9ceda1b080d1d5e52
alexandru <boncut.alex10@gmail.com>
2021-12-26 22:59:20 -0600

    Remove index action. Refactor show/create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,22 @@
 class UsersController < ApplicationController
-
-  def index
-    user = User.all
-    render json: user
-  end
+  skip_before_action :authorized?, only: [:create]
 
   def show
-    user = User.find(params[:user_id])
-    render json: user
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      token = encode_token(user_id: user.id)
+      render json: { user: UserSerializer.new(user), jwt: token}, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :unprocessable_entity
+    end
   end
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :bio, :birthdate, :location, :name)
+    params.require(:user).permit(:username, :password, :password_confirmation, :email, :bio, :birthdate, :location, :name)
   end
 end
