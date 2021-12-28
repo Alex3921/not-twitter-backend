c0e88eeae29311a4e0762987109952b73f568d27
alexandru <boncut.alex10@gmail.com>
2021-12-27 23:43:14 -0600

    Remove nested routes :posts, :likes from :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show] do
-    resources :posts, only: [:index, :show, :create]
-    resources :likes, only: [:index]
-  end
+  resources :users, only: [:show]
+  resources :posts, only: [:index, :show, :create]
+  resources :likes, only: [:index]
 
   # get     'signup',       to: 'users#new'
   post    'signup',       to: 'users#create'
   # get     'login',        to: 'auth#new'
-  post    'login',        to: 'auth#create'
+  post    'signin',        to: 'auth#create'
   # delete  'logout',       to: 'sessions#destroy'
 end
