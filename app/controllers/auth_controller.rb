fa9d4712640b90b84a677c9bf9f0cff8712398bc
alexandru <boncut.alex10@gmail.com>
2021-12-27 23:40:22 -0600

    Fix typo

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,18 +2,18 @@ class AuthController < ApplicationController
   skip_before_action :authorized?, only: [:create]
 
   def create
-    @user = User.find_by(username: user_login_params[:username])
+    @user = User.find_by(username: user_signin_params[:username])
 
-    if @user && @user.authenticate(user_login_params[:password])
+    if @user && @user.authenticate(user_signin_params[:password])
       token = encode_token({ user_id: @user.id})
-      render json: {user: UserSerializer.new(user), jwt: token }, status: :accepted
+      render json: {user: UserSerializer.new(@user), jwt: token }, status: :accepted
     else
       render json: { message: "Invalid username or password" }, status: :unauthorized
     end
   end
 
   private
-  def user_login_params
+  def user_signin_params
     params.require(:user).permit(:username, :password)
   end
 end
