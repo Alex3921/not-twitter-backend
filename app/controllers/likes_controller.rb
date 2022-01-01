753069546bbba0c8c07817f7a6209d62063fc1c7
alexandru <boncut.alex10@gmail.com>
2021-12-31 23:55:29 -0600

    Add create action and strong params

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,5 +5,15 @@ class LikesController < ApplicationController
     render json: { liked_posts: liked_posts_serialized }, status: :accepted
   end
 
+  def create
+    liked_post = current_user.liked.build(like_params)
+    liked_post.save
+    render json: { message: "Post was added to favorites!" }, status: :created
+  end
+
+  private
+  def like_params
+    params.require(:likes).permit(:post_id)
+  end
   
 end
