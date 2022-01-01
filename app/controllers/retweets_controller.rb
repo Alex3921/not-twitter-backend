be22987ee34113d1acfd467194ec5b7823ac2ba8
alexandru <boncut.alex10@gmail.com>
2021-12-31 23:56:16 -0600

    Add create action and strong params

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -1,2 +1,14 @@
 class RetweetsController < ApplicationController
+  def create
+    retweet_post = current_user.retweets.build(retweet_params)
+    retweet_post.save
+    author_name = Post.find(retweet_post.post_id).author.name
+
+    render json: { message: "You shared #{author_name}'s post!" }, status: :created
+  end
+
+  private
+  def retweet_params
+    params.require(:retweet).permit(:post_id)
+  end
 end
