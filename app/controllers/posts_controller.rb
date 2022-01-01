f0909474e90331569f0ad7628fcbcf2dc8523d03
alexandru <boncut.alex10@gmail.com>
2021-12-31 23:55:57 -0600

    Add strong params and create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
+  def home
+    followed_users_posts = current_user.followed_users_posts.map{|p| PostSerializer.new(p)}
+    render json: { homePosts: followed_users_posts }, status: :accepted
+  end
 
-  def index
-    followed_users_posts = current_user.followed_users_posts
-    render json: followed_users_posts
+  def profile
+    user_posts = current_user.posts.map{|p| PostSerializer.new(p)}
+    render json: { userPosts: user_posts }, status: :accepted
   end
 
   def show
@@ -10,7 +14,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    byebug
+    post = current_user.posts.build(post_params)
+    post.save
+    render json: {post: PostSerializer.new(post)}, status: :created
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 
 end
