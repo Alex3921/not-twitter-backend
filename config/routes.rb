b6859e268197bbf0502edad63244dcf7802caf89
alexandru <boncut.alex10@gmail.com>
2022-01-02 23:46:31 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
-  resources :posts, only: [:index, :show, :create]
-  resources :likes, only: [:index]
-
-  # get     'signup',       to: 'users#new'
-  post    'signup',       to: 'users#create'
-  # get     'login',        to: 'auth#new'
-  post    'signin',        to: 'auth#create'
+  resources :users,     only: [:show]
+  resources :posts,     only: [:show, :create]
+  resources :likes,     only: [:index]
+  resources :bookmarks, only: [:index, :create]
+  resources :retweets,  only: [:create]
+  
+  get       '/profile', to: 'posts#profile'
+  get       '/home',    to: 'posts#home'
+  post      '/signup',  to: 'users#create'
+  post      '/signin',  to: 'auth#create'
   # delete  'logout',       to: 'sessions#destroy'
 end
