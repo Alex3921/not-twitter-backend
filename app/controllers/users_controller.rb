0204fe8ac62db63add126bfb23af242d9fa20bef
alexandru <boncut.alex10@gmail.com>
2022-01-03 23:35:34 -0600

    Add show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,20 @@
-class UsersController < ApplicationController
-  skip_before_action :authorized?, only: [:create]
-
-  def show
-    render json: { user: UserSerializer.new(current_user) }, status: :accepted
-  end
+class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
   def create
-    user = User.create(user_params)
-    if user.valid?
-      token = encode_token(user_id: user.id)
-      render json: { user: UserSerializer.new(user), jwt: token}, status: :created
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :unprocessable_entity
     end
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation, :email, :bio, :birthdate, :location, :name)
+    params.require(:user).permit(:username, :password, :bio, :avatar)
   end
 end
+
