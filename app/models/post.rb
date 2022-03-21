0c8bd992f373fb34187fe4aa1ef031a20ad08f17
Alexandru Boncut <boncut.alex10@gmail.com>
2022-03-20 23:57:14 -0500

    Create add_bookmark method

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
   has_many :replies, class_name: "Post", foreign_key: :in_reply_to_post_id
   has_many :likes
   has_many :retweets
+
+  def add_bookmark(current_user, bookmark_params)
+    bookmarked_post = current_user.bookmarks.build(bookmark_params)
+    bookmarked_post.save
+  end
 end
